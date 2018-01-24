546dd9b3173f61d52b9e5f5b50d3716bbcbb72ed
Roshin Rajan Panackal <roshin.panackal@muzzley.com>
2018-01-24 10:36:58 +0000

    Solid() is a function than returns object of implementor class

--- base/solid.py
+++ base/solid.py
@@ -5,17 +5,20 @@ import inspect,logging
 
 logger = logging.getLogger(__name__)
 
-try:
-    spec = util.spec_from_file_location("implementor",settings.skeleton_path)
-    module = util.module_from_spec(spec)
-    spec.loader.exec_module(module)
+def Solid():
+    try:
+        spec = util.spec_from_file_location("implementor",settings.skeleton_path)
+        module = util.module_from_spec(spec)
+        spec.loader.exec_module(module)
 
-    for name, obj in inspect.getmembers(module):
-        if inspect.isclass(obj):
-            if inspect.isabstract(obj) != True:
-                if issubclass(obj,skeleton.Skeleton):
-                    Solid = obj
-except Exception as ex:
-    logger.critical("Failed to find Skeleton implementer class")
-    logger.trace(ex)
-    exit()
+        for name, obj in inspect.getmembers(module):
+            if inspect.isclass(obj):
+                if inspect.isabstract(obj) != True:
+                    if issubclass(obj,skeleton.Skeleton):
+                        return obj()
+    except Exception as ex:
+        logger.critical("Failed to find Skeleton implementer class")
+        logger.trace(ex)
+        exit()
+
+
