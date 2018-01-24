94dfc143434b33c1af5ceb2eaa9277651ee60e83
Cristina Lopes <cristina.lopes@muzzley.com>
2018-01-24 19:50:42 +0000

    Fix on publisher

--- base/paho_mqtt.py
+++ base/paho_mqtt.py
@@ -155,6 +155,7 @@ def publisher(io,data,case=None,topic=None):
             if all (key in case for key in ("device_id","component","property")):
                 channel_id = db.get_key(case["device_id"])
                 topic = "/"+settings.api_version+"/channels/"+channel_id+"/components/"+case["component"]+"/properties/"+case["property"]+"/value"
+                logger.debug("Mqtt - Created a topic {}".format(topic))
             else:
                 logger.warning("Mqtt - Invalid arguements provided to publisher.")
                 raise Exception
