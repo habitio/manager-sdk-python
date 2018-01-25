f7f9ed4cf11a80a409916e594daa43350e8b16f7
Cristina Lopes <cristina.lopes@muzzley.com>
2018-01-25 14:30:55 +0000

    added logs to kickoff and called kickoff on run directly

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from base import app
 from base.settings import settings
+from base.views import kickoff
 import signal
 
 def signal_handler(signal, frame):
@@ -10,7 +11,8 @@ signal.signal(signal.SIGINT, signal_handler)
 if __name__ == "__main__":
     try:
         #Initial setup of Manager
-        app.kickoff()
+        print("Initial setup of Manager ...")
+        kickoff()
         app.run()
     except SystemExit:
         app.shutdown()
@@ -19,3 +21,6 @@ if __name__ == "__main__":
     except Exception:
         print("********* Unknown Error!!! ********")    
         raise
+
+
+        
