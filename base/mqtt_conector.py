5dd35e22a498a2d111bf27e36a561eab1c3c4109
Cristina Lopes <cristina.lopes@muzzley.com>
2018-01-29 18:37:00 +0000

    added logs

--- base/mqtt_conector.py
+++ base/mqtt_conector.py
@@ -18,7 +18,7 @@ class MqttConector():
         self.mqtt_client.on_message = self.on_message
         self.mqtt_client.on_disconnect = self.on_disconnect
         self.mqtt_client.on_publish = self.on_publish
-        # self.mqtt_client.on_log = self.on_log
+        self.mqtt_client.on_log = self.on_log
 
     def on_connect(self, client, userdata, flags, rc):
         try:
@@ -118,7 +118,7 @@ class MqttConector():
         else:
             logger.error("Mqtt - Expected disconnection.")
 
-    def on_log(self, userdata, level, buf):
+    def on_log(client, userdata, level, buf):
         logger.debug("Mqtt - Paho log: {}".format(buf))
 
     def mqtt_config(self):
