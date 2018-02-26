96608775f5b546874ec548039f6158023ce429cf
Roshin Rajan Panackal <roshin.panackal@muzzley.com>
2018-02-26 15:25:56 +0000

    New 3 functions added to interface

--- base/skeleton.py
+++ base/skeleton.py
@@ -157,7 +157,7 @@ class Skeleton(ABC):
     def get_channel_template(self,channel_id):
         """
         Input : 
-        channel_id - channel_id of the device.
+            channel_id - channel_id of the device.
 
         Returns channel_template_id
 
@@ -186,7 +186,27 @@ class Skeleton(ABC):
         except Exception as ex:
             logger.debug("\n"+json.dumps(resp.json(),indent=4,sort_keys=True)+"\n")
             logger.trace(str(ex))
-        
+    
+    def get_device_id(self,channel_id):
+        """
+        To retrieve device_id using channel_id
+
+        """
+        if self.exists(channel_id):
+            return self.retrieve(channel_id)
+        else:
+            return None
+
+    def get_channel_id(self,device_id):
+        """
+        To retrieve channel_id using device_id
+
+        """
+        if self.exists(device_id):
+            return self.retrieve(device_id)
+        else:
+            return None
+
     def store(self,key,value):
         """
         To store a value to database with a unique identifier called key
@@ -206,7 +226,7 @@ class Skeleton(ABC):
         To check if a key is already present in database.
 
         """
-        return db.had_key(key)
+        return db.has_key(key)
 
     def log(self,message,level):
         """
@@ -257,3 +277,32 @@ class Skeleton(ABC):
         logger.debug("Will publisher to mqtt")
         mqtt.publisher(io="iw",data=data,case=case)
         
+    def renew_credentials(self,sender,credentials,rub=False):
+        """
+        To update credentials in database
+            channel_id - channel_id of the device.
+            credentials - a dictionary with data to be updated. 
+            sender      - a dictionary with keys 'owner_id' and 
+                        'client_id'.
+            rub         - flag variable , 'False' by default.
+                            if 'True'  - overwrites entire credentials.
+                            if 'False' - overwrites specific data in credentials.
+        """
+        try:
+            key = "/".join([
+                sender["client_id"],
+                sender["owner_id"]
+            ])
+
+            if self.exists(key):
+                original = self.retrieve(key)
+                if rub:
+                    self.store(key,credentials)
+                else:
+                    original.update(credentials)
+                    self.store(key,original)
+                logger.debug("Credentials successfully renewed : "+str(key)+" !")
+            else:
+                logger.debug("Invalid data provided for renewing credentials : "+str(key))
+        except Exception as ex:
+            logger.debug("Renew credentials failed!!! \n"+str(ex))
