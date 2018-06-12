19430016831b968bde362f7c6ed9a7b17eba8926
Cristina Lopes <cristina.lopes@muzzley.com>
2018-06-12 19:04:28 +0100

    Moved app to init

--- base/views.py
+++ base/views.py
@@ -4,8 +4,8 @@ from base import logger
 from base.mqtt_connector import mqtt 
 from base.webhook_handler import webhook
 from base.settings import settings
-# from sdk.run import app
-from base import app
+from sdk.run import app
+# from base import app
 
 def kickoff():
     '''
--- run.py
+++ run.py
@@ -1,7 +1,24 @@
 from base.settings import settings
 from base import logger
 
-from base import app
+
+from flask import Flask
+from flask_mqtt import Mqtt
+
+# Flask App
+logger.verbose("Creating Flask Object...")
+try:
+    app = Flask(__name__, instance_relative_config=True)
+    logger.info("Flask object successfully created!")
+except Exception as ex:
+    logger.emergency("Flask object creation failed ...")
+    logger.trace(ex)
+    raise
+
+app.config.from_object("flask_config")
+
+from base import views
+
 print("__name__ = "+__name__)
 if __name__ == "__main__":
     try:
