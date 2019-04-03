2379f469d19636b4370779cf684c571ac143a14c
Laoska <laoska.guadamuz@habit.io>
2019-04-03 17:45:31 +0100

    working on select-device

--- base/skeleton_device/webhook.py
+++ base/skeleton_device/webhook.py
@@ -5,12 +5,13 @@ import traceback
 import os
 from flask import Response, request
 from tenacity import retry, wait_fixed
+import concurrent
+import asyncio
 
 from base import settings
 from base.common.webhook_base import WebhookHubBase
-from base.redis_db import get_redis
 from base.utils import format_str
-from base.constants import DEFAULT_RETRY_WAIT
+from base.constants import DEFAULT_RETRY_WAIT, DEFAULT_THREAD_MAX_WORKERS
 
 from .polling import PollingManager
 from .token_refresher import TokenRefresherManager
@@ -120,92 +121,26 @@ class WebhookHubDevice(WebhookHubBase):
                 else:
                     return Response(status=422)
 
+                owner_id = request.headers["X-Owner-Id"]
+                client_id = request.headers["X-Client-Id"]
+                channel_template = request.headers["X-Channeltemplate-Id"]
 
 
-                credentials = self.db.get_credentials(request.headers["X-Client-Id"], request.headers["X-Owner-Id"])
+                credentials = self.db.get_credentials(client_id, owner_id)
                 channels = []
 
-                for device in paired_devices:
-                    channel_id = self.db.get_channel_id(device["id"])
-                    if channel_id:
-                        logger.info("Channel already in database")
-                        channel = {
-                            "id": channel_id
-                        }
+                loop = asyncio.get_event_loop()
+                channels = loop.run_until_complete(
+                    asyncio.gather(self.send_channel_requests(paired_devices, credentials, client_id, owner_id, channel_template)))
+                loop.close()
 
-                        # Validate if still exists on Muzzley
-                        url = "{}/channels/{}".format(settings.api_server_full, channel["id"])
-                        resp = self.session.get(url, data=None)
-                        logger.verbose("Received response code[{}]".format(resp.status_code))
-
-                        if int(resp.status_code) not in (200, 201):
-                            channel = self.create_channel_id(device)
-                        else:
-                            logger.info("Channel still valid in Muzzley")
-
-                            # Ensure persistence of manufacturer's device id (key) to channel id (field) in redis hash
-                            self.db.set_channel_id(device["id"], channel_id, True)
-                            logger.verbose("Channel added to database")
-                    else:
-                        channel = self.create_channel_id(device)
-
-
-                    # Granting permission to intervenient with id X-Client-Id
-                    url = "{}/channels/{}/grant-access".format(settings.api_server_full, channel["id"])
-
-                    try:
-                        data = {
-                            "client_id": request.headers["X-Client-Id"],
-                            "role": "application"
-                        }
-
-                        logger.debug("Initiated POST - {}".format(url))
-                        logger.verbose(format_str(data, is_json=True))
-
-                        resp1 = self.session.post(url, json=data)
-                        logger.debug("Received response code[{}]".format(resp1.status_code))
-
-                        if int(resp1.status_code) not in (201, 200):
-                            logger.debug(format_str(resp1.json(), is_json=True))
-                            raise Exception
-                    except:
-                        logger.error("Failed to grant access to client {} {}".format(
-                            request.headers["X-Client-Id"],
-                            traceback.format_exc(limit=5))
-                        )
-                        return Response(status=400)
-
-                    # Granting permission to intervenient with id X-Owner-Id
-                    try:
-                        data = {
-                            "client_id": request.headers["X-Owner-Id"],
-                            "requesting_client_id": request.headers["X-Client-Id"],
-                            "role": "user"
-                        }
-
-                        logger.debug("Initiated POST - {}".format(url))
-                        logger.verbose(format_str(data, is_json=True))
-
-                        resp2 = self.session.post(url, json=data)
-                        logger.verbose("Received response code[{}]".format(resp2.status_code))
-
-                        if int(resp2.status_code) not in (201, 200):
-                            logger.debug(format_str(resp2.json(), is_json=True))
-                            raise Exception
-                    except:
-                        logger.error("Failed to grant access to owner {} {}".format(
-                            request.headers["X-Owner-Id"],
-                            traceback.format_exc(limit=5)))
-                        return Response(status=400)
-
-                    channels.append(channel)
-                    self.db.set_credentials(credentials, request.headers["X-Client-Id"], request.headers["X-Owner-Id"],
-                                   channel["id"])
+                for channel in channels:
+                    logger.info(channel)
 
                 sender = {
-                    "channel_template_id": request.headers["X-Channeltemplate-Id"],
-                    "client_id": request.headers["X-Client-Id"],
-                    "owner_id": request.headers["X-Owner-Id"]
+                    "channel_template_id": channel_template,
+                    "client_id": client_id,
+                    "owner_id": owner_id
                 }
 
                 self.implementer.did_pair_devices(sender=sender, credentials=credentials, paired_devices=paired_devices)
@@ -223,39 +158,142 @@ class WebhookHubDevice(WebhookHubBase):
 
         return Response(status=403)
 
-    def create_channel_id(self, device):
+    async def send_channel_requests(self, devices, credentials, client_id, owner_id, channel_template):
+        loop = asyncio.get_event_loop()
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=DEFAULT_THREAD_MAX_WORKERS) as executor:
+            futures = [
+                loop.run_in_executor(
+                    executor,
+                    self.channels_grant,
+                    device, credentials, client_id, owner_id, channel_template
+                )
+                for device in devices
+            ]
+
+            return futures
+
+    def channels_grant(self, device, credentials, client_id, owner_id, channel_template):
+
+        try:
+            channel = self.get_or_create_channel(device, channel_template)
+
+            # Granting permission to intervenient with id X-Client-Id
+            url = "{}/channels/{}/grant-access".format(settings.api_server_full, channel["id"])
+
+            try:
+                data = {
+                    "client_id": client_id,
+                    "role": "application"
+                }
+
+                logger.debug("Initiated POST - {}".format(url))
+                logger.verbose(format_str(data, is_json=True))
+
+                resp_app = self.session.post(url, json=data)
+                logger.debug("Received response code[{}]".format(resp_app.status_code))
+
+                if resp_app.status_code not in (201, 200):
+                    logger.debug(format_str(resp_app.json(), is_json=True))
+                    return False
+            except:
+                logger.error("Failed to grant access to client {} {}".format(
+                    client_id,
+                    traceback.format_exc(limit=5))
+                )
+                return False
+
+            # Granting permission to intervenient with id X-Owner-Id
+            try:
+                data = {
+                    "client_id": owner_id,
+                    "requesting_client_id": client_id,
+                    "role": "user"
+                }
+
+                logger.debug("Initiated POST - {}".format(url))
+                logger.verbose(format_str(data, is_json=True))
+
+                resp_user = self.session.post(url, json=data)
+                resp_user.verbose("Received response code[{}]".format(resp_user.status_code))
+
+                if resp_user.status_code not in (201, 200):
+                    logger.debug(format_str(resp_user.json(), is_json=True))
+                    return False
+            except:
+                logger.error("Failed to grant access to owner {} {}".format(
+                    channel_template,
+                    traceback.format_exc(limit=5)))
+                return False
+
+            self.db.set_credentials(credentials, client_id, owner_id, channel["id"])
+            return channel
+
+        except Exception as e:
+            logger.error('Error while requesting grant {}'.format(e))
+
+        return None
+
+
+    def get_or_create_channel(self, device, channel_template):
+        try:
+            channel_id = self.db.get_channel_id(device["id"])
+
+            if channel_id:
+                logger.info("Channel already in database")
+                return {
+                    "id": channel_id
+                }
+
+            # Validate if still exists on Muzzley
+            url = "{}/channels/{}".format(settings.api_server_full, channel_id)
+            resp = self.session.get(url, data=None)
+
+            logger.verbose("/v3/channels/{} response code {}".format(channel_id, resp.status_code))
+
+            if resp.status_code not in (200, 201):
+                channel_id = self.create_channel_id(device, channel_template)
+
+            # Ensure persistence of manufacturer's device id (key) to channel id (field) in redis hash
+            self.db.set_channel_id(device["id"], channel_id, True)
+            logger.verbose("Channel added to database {}".format(channel_id))
+
+            return {
+                "id": channel_id
+            }
+
+        except Exception as e:
+            logger.error('Error get_or_create_channel {}'.format(e))
+
+        return None
+
+    def create_channel_id(self, device, channel_template):
 
         # Creating a new channel for the particular device"s id
         data = {
             "name": "Device" if not "content" in device else device["content"],
-            "channeltemplate_id": request.headers["X-Channeltemplate-Id"]
+            "channeltemplate_id": channel_template
         }
-        url = settings.api_server_full
 
         try:
-            logger.debug("Initiated POST - {}".format(url))
+            logger.debug("Initiated POST - {}".format(settings.api_server_full))
             logger.verbose(format_str(data, is_json=True))
 
-            resp = self.session.post("{}/managers/self/channels".format(url), json=data)
+            resp = self.session.post("{}/managers/self/channels".format(settings.api_server_full), json=data)
 
             logger.debug("Received response code[{}]".format(resp.status_code))
 
-            if int(resp.status_code) != 201:
+            if resp.status_code != 201:
                 logger.debug(format_str(resp.json(), is_json=True))
                 raise Exception
 
         except Exception as ex:
             logger.error(
-                "Failed to create channel for channel template {} {}".format(request.headers["X-Channeltemplate-Id"],
+                "Failed to create channel for channel template {} {}".format(channel_template,
                                                                              traceback.format_exc(limit=5)))
             return Response(status=400)
 
-        # Ensure persistence of manufacturer"s device id (key) to channel id (field) in redis hash
-        logger.verbose("Channel added to database")
-        self.db.set_channel_id(device["id"], resp.json()["id"], True)
-
-        channel = resp.json()
-        return channel
+        return resp.json()["id"]
 
     @retry(wait=wait_fixed(DEFAULT_RETRY_WAIT))
     def patch_endpoints(self):
