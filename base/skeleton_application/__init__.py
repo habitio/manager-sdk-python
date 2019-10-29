561c1951a95409ba5e03bfc98ef74578dc57cda0
Tiago Gomes <tiagoslg@gmail.com>
2019-10-29 12:04:27 +0000

    add type hints

--- base/skeleton_application/__init__.py
+++ base/skeleton_application/__init__.py
@@ -27,6 +27,7 @@ class SkeletonApplication(SkeletonBase):
             raise InvalidRequestException("Invalid quote")
 
     def _get_properties_by_quote(self, quote_id):
+        # get properties using quote_id
         _url = QUOTE_PROPERTIES_URI.format(quote_id=quote_id)
         _params = {
             "namespace": f"in/{NAMESPACES_FILTER}/j"
@@ -40,7 +41,15 @@ class SkeletonApplication(SkeletonBase):
 
         return properties
 
-    def _patch_property(self, quote_id, property_id, data, return_property=False):
+    def _patch_property(self, quote_id: str, property_id: str, data: dict, return_property: bool = False) -> dict:
+        """
+        Make PATCH request to update quote/<quote_id>/properties/<property_id>
+        :param quote_id: UUID
+        :param property_id: UUID
+        :param data: Dict - Payload to be patched to property
+        :param return_property: bool - True return full updated property / False return just patch response
+        :return: Dict with full updated property or PATCH response
+        """
         self.log(f"Patch property: {property_id}; Data: {data}", 7)
         url = f"{QUOTE_PROPERTIES_URI.format(quote_id=quote_id)}/{property_id}"
 
@@ -59,7 +68,14 @@ class SkeletonApplication(SkeletonBase):
 
         return resp.json()
 
-    def _patch_quote(self, quote_id, data, return_quote=False):
+    def _patch_quote(self, quote_id: str, data: dict, return_quote: bool = False) -> dict:
+        """
+        Make PATCH request to update quote/<quote_id>
+        :param quote_id: UUID
+        :param data: Dict - Payload to be patched to property
+        :param return_quote: bool - True return full updated property / False return just patch response
+        :return: Dict with full updated quote or PATCH response
+        """
         self.log(f"Patch quote: {quote_id}; Data: {data}", 7)
         url = QUOTE_URI.format(quote_id=quote_id)
 
@@ -78,7 +94,14 @@ class SkeletonApplication(SkeletonBase):
 
         return resp.json()
 
-    def update_quote_state(self, quote_id, state: str, return_quote):
+    def update_quote_state(self, quote_id: str, state: str, return_quote: bool):
+        """
+        Update quote state according to received state
+        :param quote_id: UUID
+        :param state: str from list VALID_QUOTE_STATES
+        :param return_quote: bool - True return full updated quote / False return just patch response
+        :return:
+        """
         state = state.lower()
         if state not in VALID_QUOTE_STATES:
             logger.error(f"Invalid quote state: {state}")
@@ -88,7 +111,38 @@ class SkeletonApplication(SkeletonBase):
         quote = self._patch_quote(quote_id, data, return_quote)
         return quote
 
-    def quote_simulate(self, service_id, quote_id):
+    def update_quote_property(self, quote_id: str, property_: dict, new_value: str, return_property: bool) -> dict:
+        """
+        Update quote property data according to received value
+        :param quote_id: UUID
+        :param property_: dict -> complete property object
+        :param new_value: str -> new value to property data
+        :param return_property: bool - True return full updated quote property /
+                                       False return object with property data, id and namespace
+        :return:
+        """
+        self.log(f"Update property value for {property_.get('id')}; New value: {new_value}", 7)
+        ret = {
+            "data": property_.get('data'),
+            "id": property_.get('id'),
+            "namespace": property_.get('namespace'),
+        }
+        if property_.get('data') != new_value:
+            data = {
+                'data': new_value
+            }
+            property_id = property_.get('id')
+
+            # try to patch property with new data
+            new_property = self._patch_property(quote_id, property_id, data, return_property)
+            if return_property:
+                ret = new_property
+            else:
+                ret['data'] = new_value
+
+        return ret
+
+    def quote_simulate(self, service_id: str, quote_id: str):
         """
         Invoked when application receives a quote_simulate call
 
